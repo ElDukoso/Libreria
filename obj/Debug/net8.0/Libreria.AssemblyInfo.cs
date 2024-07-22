b481f70b224b715b64c7c94fb79787721615e534
Dukoso <erickkirch85@gmail.com>
2024-07-22 17:02:50 -0400

    proyecto casi al 100%

--- obj/Debug/net8.0/Libreria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Libreria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Libreria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d902be1194b5fa48cefa0c9dafa5a47fae55420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d94f199a98fdf0a6c0cff8ff321704914bf44a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Libreria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Libreria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
